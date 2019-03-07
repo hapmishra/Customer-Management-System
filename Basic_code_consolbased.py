81e08f0b481d1c8e04e596a1e4de553e7a1a2696
hapmishra <happymishra25@outlook.com>
2019-03-07 16:56:22 +0530

    This is the basic code for the management system which is console based only but having almost every functionality of the actual management system. Pros: 1.Consisting almost every feature of our actual project:   a.Add Customer   b.Update Customer   c.Search Customer   d.Remove Customer   e.Display All Cons: 1.This is console based as it don't have interface(work is under process for the system) 2.this is initial stage for the project and at that it is not connect to any server or database and also don't having the File Handling.(testing is under process for connectivity to web but File handling is completed)

---
+++ Basic_code_consolbased.py
@@ -0,0 +1,98 @@
+namelist=[]
+agelist=[]
+idlist=[]
+moblist=[]
+index=0
+while(1):
+    print("Welcome to the Customer Management System")
+    print("Please select your choice from given below : ")
+    print("1.Add Customer","2.Update Customer","3.Remove Customer","4.Search Customer","5.Display All","6.Exit",sep="\n")
+    x=int(input("Please Enter your Choice here : "))
+# ----------------------------(Add Customer)-----------------------------------------------------------------------
+    if(x==1):
+        index+=1
+        print("Enter the Details of the Customer")
+        a=input("Enter the Name of the Customer : ")
+        namelist.append(a)
+        while(1):
+            b=input("Enter the Age of the Customer : ")
+            if(b.isnumeric()):
+                agelist.append(b)
+                break
+            else:
+                print("Please Enter Age only in Numeric Form")
+        while(1):
+            c=input("Please Enter your 10 Digit Mobile Number : ")
+            if(c.isnumeric()):
+                moblist.append(c)
+                break
+            else:
+                print("Please enter a valid mobile number")
+        idlist.append(index)
+    print(namelist,agelist,idlist,moblist,sep='\n')
+# ----------------------(update Customer)------------------------------------------------------------------------------
+    if(x==2):
+        while(1):
+            id=input("Enter Customer ID :")
+            if(id.isnumeric()):
+                id=int(id)
+                namelist.pop(id)
+                agelist.pop(id)
+                moblist.pop(id)
+                a = input("Enter the Name : ")
+                namelist.insert(id,a)
+                while(1):
+                    b = input("Enter the Age of Customer")
+                    if(b.isnumeric()):
+                        b=int(b)
+                        agelist.insert(id,b)
+                        break
+                    else:
+                        print("Please enter valid age")
+                while(1):
+                    c = input("Enter Mobile Number : ")
+                    if(c.isnumeric()):
+                        moblist.insert(id,c)
+                        break
+                    else:
+                        print("Please enter valid mobile number")
+                break
+# ---------------------------------------------------------------------------------------------------------------------
+# -------------------------------------(Remove Customer)---------------------------------------------------------------
+    elif(x==3):
+        while (1):
+            id = input("Enter Customer ID :")
+            if (id.isnumeric()):
+                id = int(id)
+                id-=1
+                namelist.pop(id)
+                agelist.pop(id)
+                moblist.pop(id)
+                idlist.pop(id)
+                print("Customer Removed Sucessfully")
+                break
+            else:
+                print("Please Enter Valid Customer ID")
+# ---------------------------------------------------------------------------------------------------------------------
+# -----------------------------------(Search Customer)-----------------------------------------------------------------
+    elif (x==4):
+        while (1):
+            id = input("Enter Customer ID :")
+            if (id.isnumeric()):
+                id=int(id)
+                index=idlist.index(id)
+                name=namelist[index]
+                age=agelist[index]
+                mob=moblist[index]
+                print("Name :",name,"Age : ",age,"Mobile No. : ",mob,sep="\n")
+                break
+            else:
+                print("The Please enter a valid ID")
+# ---------------------------------------------------------------------------------------------------------------------
+# -----------------------------(Display All)---------------------------------------------------------------------------
+    elif(x==5):
+        for i in range(len(idlist)):
+            print("Cus. Name :",namelist[i],"Cus. Age :",agelist(i),"Cus. ID :",idlist(i),"Cus. Mobile :",moblist(i))
+    elif(x==6):
+        print("Thankyou")
+# ---------------------------------------------------------------------------------------------------------------------
